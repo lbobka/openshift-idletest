112f0757c44fed0bd0e1ef99102437a1a183a6be
Lars B <lars.bobka@web.de>
2019-01-18 09:50:23 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+PARA=$1
+
+if ! [ -z "$APP_PARAMS" ]; then
+        PARA=$APP_PARAMS
+        echo $PARA
+fi
+
+
 num=0
 
 if ! [ $1 -eq 0 ]; then
@@ -9,9 +17,9 @@ fi
 
 seconds_running=5
 
-while [ $num -le $1 ]
+while [ $num -le $PARA ]
 do
-        if ! [ $1 -eq 0 ]; then
+        if ! [ $PARA -eq 0 ]; then
                 let num++
         fi
         sleep 5
