05f8c6d75090db1caf54c66cfc3b2aa1afc2faf7
Lars B <lars.bobka@web.de>
2019-01-18 09:41:34 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
 
-while [ $num -le 5 ]
+num=0
+
+if ! [ $1 -eq 0 ]; then
+        num=1
+fi
+
+seconds_running=5
+
+while [ $num -le $1 ]
 do
-   echo "$num"
-      if [condition];then
-      let num++
-   fi
+        if ! [ $1 -eq 0 ]; then
+                let num++
+        fi
+        sleep 5
+        echo "idletest is running since:" $seconds_running "seconds"
+        seconds_running=$[$seconds_running+5]
 done
